ae2b3d08559f7b941e0001465f134393b81d709c
Mike Cooper <shivagogo@gmail.com>
2015-11-03 15:03:49 -0500

    Relate contact

---
+++ contact.rb
@@ -0,0 +1,31 @@
+class Contact
+	attr_reader :id
+	attr_accessor :first_name, :last_name, :email, :notes
+
+	@@contacts = []
+	@@id = 1
+
+	def initialize(first_name, last_name, email, notes = nil) #(options)
+		@first_name = first_name #options[:first_name]
+		@last_name = last_name
+		@email = email
+		@notes = notes
+		@id = @@id
+		@@id += 1
+	end
+
+	def self.create(first_name, last_name, email, notes = nil)
+		new_contact = new(first_name, last_name, email, notes = nil)
+		#new_contact.id = @@id
+		@@contacts << new_contact
+	end
+
+	def self.all
+		@@contacts
+	end
+
+	def full_name
+		"#{first_name} #{last_name}"
+	end
+
+end
